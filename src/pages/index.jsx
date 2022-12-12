dfecab040843c0f86975fd6af67a09f522c85742
abdelrahman-khaled-nvtc <118051713+abdelrahman-khaled-nvtc@users.noreply.github.com>
2022-12-12 10:58:14 +0300

    updated the name

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -255,7 +255,7 @@ export default function Home({ articles }) {
         </title>
         <meta
           name="description"
-          content="I’m Spencer, a software designer and entrepreneur based in New York City. I’m the founder and CEO of Planetaria, where we develop technologies that empower regular people to explore space on their own terms."
+          content="I’m Abdelrahman, a software designer and entrepreneur based in New York City. I’m the founder and CEO of Planetaria, where we develop technologies that empower regular people to explore space on their own terms."
         />
       </Head>
       <Container className="mt-9">
